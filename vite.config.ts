8396ac9ad0ea305a653bd8d62b772aeac3626c8f
lanju <lan_ju@kingdee.com>
2024-11-26 11:59:50 +0800

    差不多了

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,35 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        "ehai-helper": './src/main.ts',
+      },
+      output: {
+        entryFileNames: '[name].js', // 入口文件名
+        chunkFileNames: '[name].js', // 代码分块文件名
+        assetFileNames: '[name].[ext]',// 资源文件名
+      }
+    }
+  },
   plugins: [
     vue(),
     vueJsx(),
     vueDevTools(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: './dist/ehai-helper.js', // 复制到输出目录的目标位置
+          dest: '../chrome-extension-template/', // 需要复制的文件或目录
+        }
+      ]
+    }),
   ],
   resolve: {
     alias: {
