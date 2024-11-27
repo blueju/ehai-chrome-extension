65492c93095cef3e950248a3e78429de58b1830a
lanju <lan_ju@kingdee.com>
2024-11-27 10:34:56 +0800

    调整 vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,56 +1,64 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
+import {viteStaticCopy} from 'vite-plugin-static-copy'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
 // import { viteMockServe } from 'vite-plugin-mock';
 
 // https://vite.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        "ehai-helper": './src/main.ts',
-      },
-      output: {
-        entryFileNames: '[name].js', // 入口文件名
-        chunkFileNames: '[name].js', // 代码分块文件名
-        assetFileNames: '[name].[ext]',// 资源文件名
-      }
-    }
-  },
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-    viteStaticCopy({
-      targets: [
-        {
-          src: './dist/ehai-helper.js', // 复制到输出目录的目标位置
-          dest: '../chrome-extension-template/', // 需要复制的文件或目录
+    build: {
+        outDir: 'dist',
+        lib: {
+            entry: './src/main.ts', // 入口文件
+            name: 'ehaiHelper',
+            fileName: (format) => `ehai-helper.js`,
+            formats: ['iife'], // 你可以指定需要的格式
+        },
+        rollupOptions: {
+            output: {
+                assetFileNames: 'ehai-helper.[ext]',// 资源文件名
+            }
         }
-      ]
-    }),
-    // viteMockServe({
-    //   mockPath: 'mock',
-    //   enable: true,
-    // }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
-  },
+    define: {
+        'process.env': {}
+    },
+    plugins: [
+        vue(),
+        vueJsx(),
+        vueDevTools(),
+        AutoImport({
+            resolvers: [ElementPlusResolver()],
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()],
+        }),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: './dist/ehai-helper.js', // 复制到输出目录的目标位置
+                    dest: '../chrome-extension-template/', // 需要复制的文件或目录
+                },
+                {
+                    src: './dist/ehai-helper.css', // 复制到输出目录的目标位置
+                    dest: '../chrome-extension-template/', // 需要复制的文件或目录
+                }
+            ]
+        }),
+        // viteMockServe({
+        //   mockPath: 'mock',
+        //   enable: true,
+        // }),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        },
+    },
 })
