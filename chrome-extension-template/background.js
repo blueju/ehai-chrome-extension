c969fbc1739c36f028d9af9bdf0029704926d6d6
lanju <lan_ju@kingdee.com>
2024-12-03 22:44:48 +0800

    不需要重定向

--- chrome-extension-template/background.js
+++ chrome-extension-template/background.js
@@ -1,28 +1,28 @@
-const blockUrls = [
-    "https://booking.1hai.cn/static/js/8.9f7dbbde.chunk.js"
-]
+// const blockUrls = [
+//     "https://booking.1hai.cn/static/js/8.9f7dbbde.chunk.js"
+// ]
 
-blockUrls.forEach((domain, index) => {
-    const id = index + 1;
-    chrome.declarativeNetRequest.updateDynamicRules({
-        addRules: [
-            {
-                "id": id,
-                "priority": 1,
-                "condition": {
-                    "urlFilter": domain,
-                    "resourceTypes": ["script"]
-                },
-                action: {
-                    type: 'redirect',
-                    redirect: {
-                        url: 'https://markdown-note-1255426737.cos.ap-guangzhou.myqcloud.com/8.9f7dbbde.chunk.js'
-                    }
-                }
-            }
-        ],
-        removeRuleIds: [id]
-    });
-});
+// blockUrls.forEach((domain, index) => {
+//     const id = index + 1;
+//     chrome.declarativeNetRequest.updateDynamicRules({
+//         addRules: [
+//             {
+//                 "id": id,
+//                 "priority": 1,
+//                 "condition": {
+//                     "urlFilter": domain,
+//                     "resourceTypes": ["script"]
+//                 },
+//                 action: {
+//                     type: 'redirect',
+//                     redirect: {
+//                         url: 'https://markdown-note-1255426737.cos.ap-guangzhou.myqcloud.com/8.9f7dbbde.chunk.js'
+//                     }
+//                 }
+//             }
+//         ],
+//         removeRuleIds: [id]
+//     });
+// });
 
 
